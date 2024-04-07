394a6e255fbc48ae6500d47b78f715ccd27c5d80
Marco <mcruzg1606@alumno.ipn.mx>
2024-04-07 17:04:19 -0600

    Menu Responsive

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@ function iniciarApp() {
         loadIonicons();
         toggleMenu();
         duplicarLogosParaCarruselInfinito();
+        activarMenuResponsive();
 }
 
 function loadIonicons() {
@@ -36,9 +37,36 @@ function duplicarLogosParaCarruselInfinito() {
     const todosLosSlides = document.querySelectorAll('.logos-slide');
 
     todosLosSlides.forEach(slide => {
-        // Clona el contenido de logos para tener el doble de elementos
         const clonado = slide.cloneNode(true);
-        slide.append(...clonado.childNodes); // Añade los clones al final del mismo contenedor
+        slide.append(...clonado.childNodes); 
     });
 }
 
+function activarMenuResponsive() {
+    const hamburgerCheckbox = document.querySelector('.hamburger input[type="checkbox"]');
+    const nav = document.querySelector('.navegacion-principal');
+    const flags = document.querySelector('#flags'); // Selector para las banderas
+
+    hamburgerCheckbox.addEventListener('change', function() {
+        if (window.innerWidth < 768) {
+            // Cambia la visualización de la navegación y las banderas basado en el checkbox
+            const displayStyle = this.checked ? 'block' : 'none';
+            nav.style.display = displayStyle;
+            flags.style.display = displayStyle; // Aplica el mismo estilo a las banderas
+        }
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth >= 768) {
+            // Restablece la visualización para pantallas más grandes
+            nav.style.display = 'flex'; // Asume que la visualización predeterminada es flex
+            flags.style.display = 'flex'; // Asume que la visualización predeterminada de las banderas es flex
+        } else {
+            // Asegura que tanto la navegación como las banderas se oculten en pantallas pequeñas si el checkbox no está marcado
+            if (!hamburgerCheckbox.checked) {
+                nav.style.display = 'none';
+                flags.style.display = 'none';
+            }
+        }
+    });
+}
