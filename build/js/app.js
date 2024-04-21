3d14f562d9b28ad6cd1c2138ff3deaca1557749e
Marco <mcruzg1606@alumno.ipn.mx>
2024-04-21 15:30:58 -0600

    Ajuste para Tablets

--- build/js/app.js
+++ build/js/app.js
@@ -11,6 +11,7 @@ function iniciarApp() {
         loadIonicons();
         scrollNav();
         navegacionFija();
+        scrollNavF();
 
 }
 
@@ -33,6 +34,21 @@ function navegacionFija(){
 
 function scrollNav(){
     const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click',function(e){
+            e.preventDefault();
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({behavior: 'smooth' });
+
+        });
+    });
+}
+
+function scrollNavF(){
+    const enlaces = document.querySelectorAll('.navegacion-footer a');
 
     enlaces.forEach( enlace => {
         enlace.addEventListener('click',function(e){
